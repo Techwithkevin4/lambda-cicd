62de7cf65bafb541ce1515b0334622420fcf7ffc
Kevin <elmerhern44@gmail.com>
2026-07-11 15:44:14 -0500

    updates

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return{
         'statusCode' : 200,
-        'body': json.dump('Hello from our CICD github actions workflow vscode')
+        'body': json.dump('Hello updated Lambda from vscode')
     }
